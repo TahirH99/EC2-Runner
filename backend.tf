74f1500e74e9d417ad952582756c29805eec8f04
TahirH99 <tahirhuss23@gmail.com>
2024-12-18 19:55:26 +0000

    update

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tahir23"
+    bucket         = "tahirkub"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
